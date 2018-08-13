6f314d78fb8038af3a091a66763f6d434b3a1e49
Marcos  Ferreira Severino <marcosradix@gmail.com>
2018-08-13 11:56:14 -0300

    ok

--- src/main/java/br/gov/ce/sefaz/controller/Controller.java
+++ src/main/java/br/gov/ce/sefaz/controller/Controller.java
@@ -1,29 +1,47 @@
 package br.gov.ce.sefaz.controller;
 
+import org.sonatype.plexus.components.cipher.DefaultPlexusCipher;
+import org.sonatype.plexus.components.cipher.PlexusCipherException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.gov.ce.sefaz.model.HashModel;
 
 @RestController
+@RequestMapping
 public class Controller {
 	
-	@GetMapping("/my-maven-password-hash")
-	public String seuNome(){
-		
-		return "Bem-Vindo";
-	}
+	@GetMapping(path="/my-maven-password-hash")
+	public String goHome(){
+		return "index";
+}
 	
 	
 	@PostMapping("/my-maven-password-hash")
 	public HashModel init(@RequestBody HashModel hashModel ){
-		String senhaMaster = hashModel.gerarSenhaHashMaster(hashModel.getSenhaPrincipal());
-		String senhaServer = hashModel.gerarSenhaHashServer(hashModel.getSenhaPrincipal());
-		hashModel.setSenhaPrincipal("");
-		hashModel.setSenhaMaster(senhaMaster);
-		hashModel.setSenhaServer(senhaServer);
+		HashModel hashsGerados = null;
+		try {
+			hashsGerados = gerarSenhaHash(hashModel.getSenhaPrincipal());
+		} catch (PlexusCipherException e) {
+			e.printStackTrace();
+		}
+		return hashsGerados;
+	}
+	
+	public HashModel gerarSenhaHash(String senhaPrincipal) throws PlexusCipherException{
+		DefaultPlexusCipher cipher = new DefaultPlexusCipher();
+		HashModel hashModel = new HashModel();
+		String masterPasswd = cipher.encryptAndDecorate( senhaPrincipal, "settings.security" );
+		String serverPasswd =  cipher.encryptAndDecorate( senhaPrincipal, masterPasswd );
+		
+		hashModel.setSenhaMaster(masterPasswd);
+		hashModel.setSenhaServer(serverPasswd);
 		return hashModel;
 	}
 }
+
+
+
--- src/main/java/br/gov/ce/sefaz/model/HashModel.java
+++ src/main/java/br/gov/ce/sefaz/model/HashModel.java
@@ -1,8 +1,5 @@
 package br.gov.ce.sefaz.model;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.Serializable;
 
 public class HashModel implements Serializable {
@@ -37,60 +34,4 @@ public class HashModel implements Serializable {
 				+ senhaServer + "]";
 	}
 	
-	   public String gerarSenhaHashMaster(String senhaDigitada){
-		   
-		   String comandoSenhaMaster = String.format( "mvn --encrypt-master-password %s" ,senhaDigitada);
-	       
-	        String saidaDeHash = "";
-	        
-	        try {
-	            Process p = Runtime.getRuntime().exec("cmd /c" + comandoSenhaMaster);
-	            BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-	            String line;
-
-	            while (true) {
-	                line = r.readLine();
-	                if (line == null) {
-	                    break;
-	                }
-	                if(line.substring(0).startsWith("{")){
-	                    System.out.println(line);
-	                    saidaDeHash = line;
-	                    
-	                }
-	            }
-	            
-	        } catch (IOException ex) {
-	           ex.printStackTrace();
-	        }
-	        return saidaDeHash;
-	    }
-	   
-	   
-	   public String gerarSenhaHashServer(String senhaDigitada){
-	       String comandoSenhaServer = String.format( "mvn --encrypt-password %s" ,senhaDigitada);
-	        String saidaDeHash = "";
-	        
-	        try {
-	            Process p = Runtime.getRuntime().exec("cmd /c" + comandoSenhaServer);
-	            BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-	            String line;
-
-	            while (true) {
-	                line = r.readLine();
-	                if (line == null) {
-	                    break;
-	                }
-	                if(line.substring(0).startsWith("{")){
-	                    System.out.println(line);
-	                    saidaDeHash = line;
-	                    
-	                }
-	            }
-	            
-	        } catch (IOException ex) {
-	           ex.printStackTrace();
-	        }
-	        return saidaDeHash;
-	    }
 }
